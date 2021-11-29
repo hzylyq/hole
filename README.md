cba1bde1de5103f50b53a54106eec871c6d6173e
hzy <164177565@qq.com>
2021-11-29 19:35:56 +0800

    fix: 命令行解析

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # hole
 
-web 脚手架
+命令行解析工具, 通过输入entity生成对应的service, repo, http handler
+
+Usege hole -entity=xxx.go -service= 
+
+天天写业务，每天都写一样的, 让人想吐, 还是写个工具让代码更容易生成吧.
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+const defaultEntity = ""
+const defaultServiceDir = "./"
+
 func main() {
+	entityFile := flag.String("entity", defaultEntity, "entity go file")
+	serviceDir := flag.String("service", defaultServiceDir, "service dir")
+	flag.Parse()
+
+	if entityFile == nil {
+		log.Fatalln("must input entity file.")
+	}
+
+	// 读取entityFile内容 解析成schema
+	curWd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("can't get current working directory: ", err)
+	}
+
+	file, err := os.Open(curWd + *entityFile)
+	if err != nil {
+		log.Fatalln("filed to read entity file")
+	}
+
+	// 解析file
+
+	log.Print(entityFile)
+	log.Print(serviceDir)
+
 	print("hello hole")
 }
